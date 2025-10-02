9f679f3d084267360188c9fa34f4233c150854dc
Francis Barla <152169724+FrancisBarla10@users.noreply.github.com>
2025-10-02 21:05:00 +0530

    Create uniquePaths.py

---
+++ uniquePaths.py
@@ -0,0 +1,52 @@
+
+def uniquePaths(grid):
+    n = len(grid)
+    m = len(grid[0])
+    
+  
+    if grid[0][0] == 1 or grid[n-1][m-1] == 1:
+        return 0
+    
+   
+    dp = [[0]*m for _ in range(n)]
+    dp[n-1][m-1] = 1
+    
+   
+    for j in range(m-2, -1, -1):
+        
+       
+        if grid[n-1][j] == 1:
+            break
+        
+       
+        else:
+            dp[n-1][j] = 1
+  
+    for i in range(n-2, -1, -1):
+        
+
+        if grid[i][m-1] == 1:
+            break
+        
+
+        else:
+            dp[i][m-1] = 1
+    
+
+    for i in range(n-2, -1, -1):
+        for j in range(m-2, -1, -1):
+            if grid[i][j] == 0:
+                
+
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
+    
+    return dp[0][0]
+
+if __name__ == "__main__":
+    grid = [
+        [ 0, 0, 0 ],
+        [ 0, 1, 0 ],
+        [ 0, 0, 0 ]
+    ]
+    
+    print(uniquePaths(grid))
